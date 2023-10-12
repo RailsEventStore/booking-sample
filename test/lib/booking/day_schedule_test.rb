3ce5e1752f4fd8cafe09b7a70bcd083930260e90
Paweł Pacana <pawel.pacana@gmail.com>
2023-10-12 13:52:34 +0200

    Refactor

--- test/lib/booking/day_schedule_test.rb
+++ test/lib/booking/day_schedule_test.rb
@@ -7,56 +7,65 @@ module Booking
     test "happy path" do
       day_schedule = DaySchedule.new(open_hours)
 
-      assert_event visit_scheduled(at_12_00..at_13_00),
-                   day_schedule.reserve(at_12_00..at_13_00)
+      assert_event visit_scheduled(slot), day_schedule.reserve(slot)
     end
 
     test "cannot reserve same slot twice" do
       day_schedule = DaySchedule.new(open_hours)
-      day_schedule.reserve(at_12_00..at_13_00)
+      day_schedule.reserve(slot)
 
       assert_raises DaySchedule::CannotReserve do
-        day_schedule.reserve(at_12_00..at_13_00)
+        day_schedule.reserve(slot)
       end
     end
 
     test "cannot reserve partially covered slot" do
       day_schedule = DaySchedule.new(open_hours)
-      day_schedule.reserve(at_12_00..at_13_00)
+      day_schedule.reserve(slot)
 
       assert_raises DaySchedule::CannotReserve do
-        day_schedule.reserve(at_12_45..at_13_45)
+        day_schedule.reserve(delay_by(45.minutes, slot))
       end
     end
 
     test "cannot reserve partially covered slot, again" do
       day_schedule = DaySchedule.new(open_hours)
-      day_schedule.reserve(at_12_00..at_13_00)
+      day_schedule.reserve(slot)
 
       assert_raises DaySchedule::CannotReserve do
-        day_schedule.reserve(at_11_45..at_12_45)
+        day_schedule.reserve(rush_by(15.minutes, slot))
       end
     end
 
     test "can reserve non-intersecting slots" do
       day_schedule = DaySchedule.new(open_hours)
-      day_schedule.reserve(at_12_00..at_12_45)
+      day_schedule.reserve(rush_by(1.hour, slot))
 
-      assert_event visit_scheduled(at_13_00..at_13_45),
-                   day_schedule.reserve(at_13_00..at_13_45)
+      assert_event visit_scheduled(slot), day_schedule.reserve(slot)
     end
 
     test "cannot reserve outside of open hours" do
       day_schedule = DaySchedule.new(open_hours)
-      open_at, close_at = open_hours.first, open_hours.last
 
-      assert_raises { day_schedule.reserve(open_at..(close_at + 1.hour)) }
+      assert_raises { day_schedule.reserve(delay_by(1.hour, open_hours)) }
     end
 
     private
 
+    def slot
+      Time.new(2023, 10, 10, 12, 00)...Time.new(2023, 10, 10, 13, 00)
+    end
+
     def open_hours
-      at_11_00..at_19_00
+      Time.new(2023, 10, 10, 11, 00)...Time.new(2023, 10, 10, 19, 00)
+    end
+
+    def delay_by(seconds, slot)
+      (slot.first + seconds)...(slot.last + seconds)
+    end
+
+    def rush_by(seconds, slot)
+      (slot.first - seconds)...(slot.last - seconds)
     end
 
     def visit_scheduled(time_range)
@@ -66,34 +75,6 @@ module Booking
           duration: time_range.last - time_range.first
         }
       )
-    end
-
-    def at_11_00
-      Time.new(2023, 10, 10, 11, 00)
-    end
-
-    def at_11_45
-      Time.new(2023, 10, 10, 11, 45)
-    end
-
-    def at_12_00
-      Time.new(2023, 10, 10, 12, 00)
-    end
-
-    def at_12_45
-      Time.new(2023, 10, 10, 12, 45)
-    end
-
-    def at_13_00
-      Time.new(2023, 10, 10, 13, 00)
-    end
-
-    def at_13_45
-      Time.new(2023, 10, 10, 13, 45)
-    end
-
-    def at_19_00
-      Time.new(2023, 10, 10, 19, 00)
     end
   end
 end
